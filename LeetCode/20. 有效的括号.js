37dfa3d9a56b54d1e08cfaaf759b5ccae31089ee
DangoSky <1341514988@qq.com>
2019-04-28 20:30:52 +0800

    add solution

--- LeetCode/20. 有效的括号.js
+++ LeetCode/20. 有效的括号.js
@@ -7,18 +7,33 @@
   注意空字符串可被认为是有效字符串。
 */
 
+// 使用正则将成对的括号替换成空字符。
+// 如果替换前后的字符串长度没有改变则退出循环判断字符串是否为空，为空的话就是有效的括号。
+var isValid = function(s) {
+  do {
+    var len = s.length;
+    s = s.replace("()", "").replace("[]", "").replace("{}", "");
+  } while(s.length !== len)
+  return s.length === 0;
+};
 
+// 使用栈
 var isValid = function(s) {
   let len = s.length;
   if(len % 2)  {
     return false;
   }
+  let map = new Map([
+    ["(",  ")"],
+    ["[", "]"],
+    ["{", "}"]
+  ]);
   let arr = [];
   for(let i=0; i<len; i++) {
     if(s[i] === '(' || s[i] === '[' || s[i] === '{') {
       arr.unshift(s[i]);
     }
-    else if((s[i] === ')' && arr[0] === '(') || (s[i] === ']' && arr[0] === '[') || (s[i] === '}' && arr[0] === '{')) {
+    else if(s[i] === map.get(arr[0])) {
       arr.shift();
     }
     else {
@@ -27,6 +42,6 @@ var isValid = function(s) {
   }
   return arr.length === 0 ? true : false;
 };
-// console.log(isValid("]]"));
-console.log(isValid(""));
+console.log(isValid("]]"));
+// console.log(isValid(""));
 // console.log(isValid("([)]"));
