909f7164484074669848252b7a5631c8013a847a
DangoSky <1341514988@qq.com>
2019-05-11 17:15:37 +0800

    add solution

--- LeetCode/92. 反转链表 II.js
+++ LeetCode/92. 反转链表 II.js
@@ -44,3 +44,28 @@ var reverseBetween = function(head, m, n) {
   fn(head, "set");
   return head;
 };
+
+// before为开始翻转的前一个结点
+// pre为要开始翻转的结点
+// next为pre的下一个结点
+// 将pre从m不断推移到n，把它连接到next后面并修改它的next指向，再将next连接到before后面。循环这个过程即可
+// 例如 1->2->3->4->5->NULL,m=2,n=4。
+// 将pre(2)放到next(3)后面，就成了 3->2，改变pre(2)的next指向就成了3-2->4->5->NULL。再把next(3)链接到before(1)后面，1->3-2->4->5->NULL，以此类推。
+var reverseBetween = function(head, m, n) {
+  // 为了避免对头结点进行特殊处理，我们通常是建立一个新结点，并把它指向head，最后再返回它的.next就可以。
+  // 此处是为了避免对从第一个结点就开始翻转的特殊处理
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let before = dummy;
+  for(let i=1; i<m; i++) {
+    before = before.next;
+  }
+  let pre= before.next;
+  for(let i=m; i<n; i++) {
+    let next = pre.next;
+    pre.next = next.next;
+    next.next = before.next;
+    before.next = next;
+  }
+  return dummy.next;
+}
