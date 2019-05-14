1e2c8a7ccf861d1f001c426c6fe33dcfa063cbb3
DangoSky <1341514988@qq.com>
2019-05-14 13:02:17 +0800

    add solution

--- LeetCode/204. 计数质数.js
+++ LeetCode/204. 计数质数.js
@@ -10,6 +10,7 @@
  * @param {number} n
  * @return {number}
  */
+
 var countPrimes = function(n) {
   // 判断是否是质数
   function judge(n) {
@@ -23,4 +24,24 @@ var countPrimes = function(n) {
     if(judge(i)) ans++;
   }
   return ans;
+};
+
+// 厄拉多塞筛法，时间复杂度O(NloglogN)
+// 从2开始遍历，把2的倍数都标记为false（不是素数）。再从2的下一位素数3开始，也标记其倍数。
+// 以此类推，最后仍为true的则为素数。
+var countPrimes = function(n) {
+  // 标记是否为素数
+  let mark = Array(n).fill(true);
+  // 存放素数
+  let primes = [];
+  let index = 0;
+  for(let i=2; i<=n; i++) {
+    if(mark[i] === true) {
+      primes[index++] = i;
+      for(let j=i+i; j<=n; j+=i) {
+        mark[j] = false;
+      }
+    }
+  }
+  return primes.length;
 };
