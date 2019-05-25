011fab20f4046da813478c07a2b4dbfabf8effa7
DangoSky <1341514988@qq.com>
2019-05-25 11:47:48 +0800

    add solution

--- LeetCode/912. 排序数组.js
+++ LeetCode/912. 排序数组.js
@@ -250,3 +250,134 @@ function radixSort(nums) {
     nums[index] = +res[index];
   }) 
 }
+
+// 计数排序
+// 以数组元素值为键，出现次数为值存进一个临时数组，最后再遍历这个临时数组还原回原数组。
+// 因为js的数组下标是以字符串形式存储的，所以计数排序可以用来排列负数，但不可以排小数
+var sortArray = function(nums) {
+  function countingSort(nums) {
+    let arr = [];
+    let max = Math.max(...nums);
+    let min = Math.min(...nums);
+    for(let i=0, len=nums.length; i<len; i++) {
+      let temp = nums[i];
+      arr[temp] = arr[temp] + 1 || 1;
+    }
+    let index = 0;
+    for(let i=min; i<=max; i++) {
+      while(arr[i] > 0) {
+        nums[index++] = i;
+        arr[i]--;
+      }
+    }
+  }
+  countingSort(nums);
+  return nums;
+}
+
+// 计数排序优化，
+// 把每一个数组元素都加上min的相反数，来避免特殊情况下的空间浪费。
+// 可以把所开的空间大小从max+1降低为max-min+1。max和min分别为数组中的最大值和最小值
+// 比如数组[103, 102, 101, 100]，普通的计数排序需要开一个长度为104的数组，而前面100个值都是undefined。使用这种优化方法后可以只开一个长度为4的数组。
+var sortArray = function(nums) {
+  function countingSort(nums) {
+    let arr = [];
+    let max = Math.max(...nums);
+    let min = Math.min(...nums);
+    // 加上最小值的相反数来缩小数组范围
+    let add = -min;
+    for(let i=0, len=nums.length; i<len; i++) {
+      let temp = nums[i];
+      temp += add;
+      arr[temp] = arr[temp] + 1 || 1;
+    }
+    let index = 0;
+    for(let i=min; i<=max; i++) {
+      let temp = arr[i+add];
+      while(temp > 0) {
+        nums[index++] = i;
+        temp--;
+      }
+    }
+  }
+  countingSort(nums);
+  return nums;
+}
+
+// 堆排序
+// 根据数组建立一个堆（类似完全二叉树），每个结点的值都大于左右结点（最大堆，通常用于升序），或小于左右结点（最小堆，通常用于降序）。
+// 对于升序排序，先构建最大堆后，交换堆顶元素（最大值）和堆底元素，每一次交换都能得到一个未有序序列的最大值。
+// 重新调整最大堆，再交换堆顶元素和堆底元素。重复n-1次后就能得到一个升序的数组
+var sortArray = function(nums) {
+  function heapSort(nums) {
+    // 调整最大堆，使index的值大于左右节点
+    function adjustHeap(nums, index, size) {
+      // 交换后可能会破坏堆结构，需要循环使得每一个父节点都大于左右结点
+      while(true) {
+        let max = index;
+        // 左节点
+        let left = index * 2 + 1;
+        // 右节点
+        let right = index * 2 + 2;
+        if(left < size && nums[max] < nums[left])  max = left;
+        if(right < size && nums[max] < nums[right])  max = right;
+        // 如果左右结点大于当前的结点则交换，并再循环一遍判断交换后的左右结点位置是否破坏了堆结构（比左右结点小了）
+        if(index !== max) {
+          [nums[index], nums[max]] = [nums[max], nums[index]];
+          index = max;
+        }
+        else {
+          break;
+        }
+      }
+    }
+    // 建立最大堆
+    function buildHeap(nums) {
+      // 注意这里的头节点是从0开始的，所以最后一个非叶子结点是 parseInt(nums.length/2)-1。
+      let start = parseInt(nums.length / 2) - 1;
+      let size = nums.length;
+      // 从最后一个非叶子结点开始调整，直至堆顶。
+      for(let i=start; i>=0; i--) {
+        adjustHeap(nums, i, size);
+      }
+    }
+    
+    buildHeap(nums);
+    // 循环n-1次，每次循环后交换堆顶元素和堆底元素并重新调整堆结构
+    for(let i=nums.length-1; i>0; i--) {
+      [nums[i], nums[0]] = [nums[0], nums[i]];
+      adjustHeap(nums, 0, i);
+    }
+  }
+  heapSort(nums);
+  return nums;
+}
+
+// 希尔排序
+// 类似插入排序，只是向前移动的步数变成gap，插入排序每次都只是向前移动1。
+// 通过某个增量gap，将整个序列分给若干组，从后往前进行组内成员的比较和交换，随后逐步缩小增量至1.
+var sortArray = function(nums) {
+  function shellSort(nums) {
+    let len = nums.length;
+    // 初始步数
+    let gap = parseInt(len / 2);
+    // 逐步缩小步数
+    while(gap) {
+      // 从第gap个元素开始遍历
+      for(let i=gap; i<len; i++) {
+        // 逐步其和前面其他的组成员进行比较和交换
+        for(let j=i-gap; j>=0; j-=gap) {
+          if(nums[j] > nums[j+gap]) {
+            [nums[j], nums[j+gap]] = [nums[j+gap], nums[j]];
+          }
+          else {
+            break;
+          }
+        }
+      }
+      gap = parseInt(gap / 2);
+    }
+  }
+  shellSort(nums);
+  return nums;
+}
