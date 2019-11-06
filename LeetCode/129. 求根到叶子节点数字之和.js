4d7c1d13a098f749e7daec058675a7bcf58c7969
dangosky <1341514988@qq.com>
2019-11-06 23:43:50 +0800

    update README
    Change-Id: Ie70839dd0627dda65496976a15e0d5d80ef14acf

---
+++ LeetCode/129. 求根到叶子节点数字之和.js
@@ -0,0 +1,46 @@
+/* 
+给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+例如，从根到叶子节点路径 1->2->3 代表数字 123。
+计算从根到叶子节点生成的所有数字之和。
+说明: 叶子节点是指没有子节点的节点。
+
+示例 1:
+输入: [1,2,3]
+    1
+   / \
+  2   3
+输出: 25
+解释:
+从根到叶子节点路径 1->2 代表数字 12.
+从根到叶子节点路径 1->3 代表数字 13.
+因此，数字总和 = 12 + 13 = 25.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var sumNumbers = function(root) {
+  let res = 0;
+  function fn(root, sum) {
+    if(root === null) return;
+    sum = sum * 10 + root.val;
+    fn(root.left, sum);
+    fn(root.right, sum);
+    if (root.left === null && root.right === null) {
+      res += sum;
+      sum -= root.val;
+      return;
+    }
+  }
+  fn(root, 0);
+  return res;
+};
